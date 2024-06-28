c6f45258d207892175fdf755f2df64207693b632
ChenChen <cc2286927@sina.cn>
2024-06-28 09:55:17 +0800

    Update diy-part2-lede.sh

--- DIY/diy-part2-lede.sh
+++ DIY/diy-part2-lede.sh
@@ -12,6 +12,10 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 # 替换终端为bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
+# dockerd、docker回滚25.0.5版本
+sed -i 's/PKG_VERSION:=[0-9]\+\.[0-9]\+\.[0-9]\+/PKG_VERSION:=25.0.5/g' feeds/packages/utils/docker/Makefile
+sed -i 's/PKG_VERSION:=[0-9]\+\.[0-9]\+\.[0-9]\+/PKG_VERSION:=25.0.5/g' feeds/packages/utils/dockerd/Makefile
+
 # Modify default NTP server
 echo 'Modify default NTP server...'
 sed -i 's/ntp.aliyun.com/ntp.ntsc.ac.cn/' package/base-files/files/bin/config_generate

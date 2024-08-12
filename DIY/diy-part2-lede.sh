12f7276cd7096f16e44117c31de8ae6a705fafba
ChenChen <cc2286927@sina.cn>
2024-08-12 15:04:50 +0800

    Update diy-part2-lede.sh

--- DIY/diy-part2-lede.sh
+++ DIY/diy-part2-lede.sh
@@ -12,20 +12,7 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 # 替换终端为bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
-# docker回滚25.0.6版本
-sed -i 's/PKG_VERSION:=\([0-9]\+\.[0-9]\+\.[0-9]\+\)/PKG_VERSION:=25.0.6/g' feeds/packages/utils/docker/Makefile
-sed -i 's/^\(PKG_HASH:=\)\([a-fA-F0-9]\{64\}\)/PKG_HASH:=c19640df369eec3a485158d7851fe647a1403009b11361c50a9ec4c58742c3c0/g' feeds/packages/utils/docker/Makefile
-sed -i 's/PKG_GIT_SHORT_COMMIT:=\([[:alnum:]]\+\)/PKG_GIT_SHORT_COMMIT:=32b99dd/g' feeds/packages/utils/docker/Makefile
-
-# dockerd回滚25.0.6版本or去版本验证
-#sed -i 's/PKG_VERSION:=\([0-9]\+\.[0-9]\+\.[0-9]\+\)/PKG_VERSION:=25.0.6/g' feeds/packages/utils/dockerd/Makefile
-#sed -i 's/^\(PKG_HASH:=\)\([a-fA-F0-9]\{64\}\)/PKG_HASH:=c06bfd5dc561b3d105488d9641ce5f8c7024130616ba393e61b2af47b5177658/g' feeds/packages/utils/dockerd/Makefile
-#sed -i 's/PKG_GIT_SHORT_COMMIT:=\([[:alnum:]]\+\)/PKG_GIT_SHORT_COMMIT:=b08a51f/g' feeds/packages/utils/dockerd/Makefile
-sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
-
-# containerd Has验证
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=skip/g' feeds/packages/utils/containerd/Makefile
-
+#首页注释
 sed -i '741a\
                 <tr><td width="33%">&#32534;&#35793;&#32773;&#58;&#32;&#27954;&#183;&#67;&#121;</td><td><a href="https://github.com/2286927/ByZhouCy-OneCloud-OpenWrt" style="color: black;" target="_blank">&#32534;&#35793;&#22320;&#22336;</a></td></tr>\
                 <tr><td width="33%">&#28304;&#30721;&#58;&#32;&#108;&#101;&#100;&#101;</td><td><a href="https://github.com/coolsnowwolf/lede" style="color: black;" target="_blank">&#28304;&#30721;&#38142;&#25509;</a></td></tr>
